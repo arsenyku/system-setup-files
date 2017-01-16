aa1213fc6d64a1ee9bce043d8d0365f8b28eb879
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-15 17:42:23 -0800

    fix location of syncfork.sh

--- scripts/syncfork.sh
+++ scripts/syncfork.sh
@@ -4,11 +4,18 @@ if [[ -z $(git status -s) ]]
 then
   echo "Syncing to upstream."
 else
+  echo 
   echo "Uncommitted changes.  Aborting sync."
   exit
 fi
 
-git fetch upstream
+if [[ 0 -ne $(git fetch upstream) ]]
+then
+  echo 
+  echo "No upstream defined.  Aborting sync."
+  exit
+fi
+
 git checkout master
 git merge upstream/master
 git push
