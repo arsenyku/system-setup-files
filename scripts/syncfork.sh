6f59bbbfe5b5eb53469b7867c4304609e1fda045
asu <arsenyk.ustaris.dev@gmail.com>
2017-05-21 11:34:50 -0700

    Sync branches after syncing master

--- scripts/syncfork.sh
+++ scripts/syncfork.sh
@@ -19,3 +19,16 @@ fi
 git checkout master
 git merge upstream/master
 git push
+
+# Get branches from upstream
+
+branchNames=`git branch -r | grep upstream | grep -v master | grep -v HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'`
+
+for branch in $branchNames 
+do
+   echo Syncing branch: $branch
+   git checkout -b $branch upstream/$branch
+   git push -u origin $branch
+done
+
+
